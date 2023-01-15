39e1520db48c3d11f91cd3868ea19e67269b5c3c
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-01-14 18:31:06 -0700

    switch example

---
+++ marks-the-perfect-shot.js
@@ -0,0 +1,20 @@
+const finalPosition = function (moves) {
+  var position = [0,0];
+  for (var move of moves) {
+    switch(move) {
+      case('north'):
+      position [1]++;
+      break;
+      case('south'):
+      position[1]--;
+      break;
+      case('east'):
+      position[0]++;
+      break;
+      case('west'):
+      position[0]--;
+      break;
+    }
+  }
+  return position;
+}
